20a5c4c0a0fd0ba2129afaa540dd9ff9b5054778
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 12:59:26 +0000

    [automatic commit] 2025-06-20 12:59:26 UTC 115.166.30.211

--- part1/data/queries.js
+++ part1/data/queries.js
@@ -1,4 +1,4 @@
-const fetch_dogs_and_owner_name = `SELECT d.name as dog_name, d.size as size, u.name as owner_username from
+const fetch_dogs_and_owner_name = `SELECT d.name as dog_name, d.size as size, u.username as owner_username from
                                     Dogs d inner join Users u on d.owner_id = u.user_id`;
 
 module.exports = {
