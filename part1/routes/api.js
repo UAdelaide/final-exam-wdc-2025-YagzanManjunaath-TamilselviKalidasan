227bd9977d9c05a7efae18c82efe989fbc27109a
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 12:41:49 +0000

    [automatic commit] 2025-06-20 12:41:48 UTC 115.166.30.211

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const { query } = require('./data/queries');
-const {} = require('./data/mysql_confi')
+const { query } = require('./data/mysql_config');
+const {fetch_dogs_and_owner_name} = require('./data/queries');
 
 /* GET All Dogs information */
 router.get('/dogs', function (req, res, next) {
