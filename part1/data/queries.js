bf14b164750c5dc09edfaa63c27056d28f8dd2cd
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 13:33:52 +0000

    [automatic commit] 2025-06-20 13:33:52 UTC ;; communications error to 208.67.222.222#53: timed out 115.166.30.211

--- part1/data/queries.js
+++ part1/data/queries.js
@@ -9,16 +9,17 @@ const fetch_open_walk_requests = `SELECT w_req.request_id, d.name as dog_name, w
                                                 inner join Users u on d.owner_id = u.user_id
                                             where w_req.status = 'open';`;
 
-                                            /**
-                                             *   {
-    "walker_username": "newwalker",
-    "total_ratings": 0,
-    "average_rating": null,
-    "completed_walks": 0
-  }
-                                             */
+/**
+ *   {
+"walker_username": "newwalker",
+"total_ratings": 0,
+"average_rating": null,
+"completed_walks": 0
+}
+ */
 
-const fetch_open_walk_requests = `SELECT w_req.request_id, d.name as dog_name, w_req.requested_time,
+const fetch_walker_summary = `SELECT w_req.request_id, d.name as dog_name, w_req.requested_time,
+;`;
 module.exports = {
     fetch_dogs_and_owner_name,
     fetch_open_walk_requests
