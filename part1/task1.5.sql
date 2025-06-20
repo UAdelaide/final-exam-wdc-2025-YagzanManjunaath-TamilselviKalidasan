16506dac82d64625b32a630658afa5f042e3cc8a
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 07:37:20 +0000

    [automatic commit] 2025-06-20 07:37:20 UTC 115.166.30.211

--- part1/task1.5.sql
+++ part1/task1.5.sql
@@ -16,4 +16,4 @@ INSERT INTO Users(username,email,password_hash,role) values ('alice123','alice@e
 INSERT INTO Users(username,email,password_hash,role) values ('bobwalker','bob@example.com','hashed123','walker');
 INSERT INTO Users(username,email,password_hash,role) values ('carol123','carol@example.com','hashed123','owner');
 INSERT INTO Users(username,email,password_hash,role) values ('batman','brucewayne@example.com','martha123','owner');
-INSERT INTO Users(username,email,password_hash,role) values ('alfred','alfred@example.com','martha123','owner');
+INSERT INTO Users(username,email,password_hash,role) values ('alfred','alfred@example.com','thomas123','walker');
