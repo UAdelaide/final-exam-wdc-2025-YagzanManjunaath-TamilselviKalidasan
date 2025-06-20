932c461b84cc81b2fff86ac087c2f52222b1cfd1
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 14:22:20 +0000

    [automatic commit] 2025-06-20 14:22:20 UTC 115.166.30.211

--- part1/data/queries.js
+++ part1/data/queries.js
@@ -34,11 +34,11 @@ const fetch_walker_summary = `
                 from Users u
                         left join WalkRatings w_rating on u.user_id = w_rating.walker_id
                         left join WalkRequests w_req on u.user_id = w_req.walker_id
-                        and w_req.
+                        and w_req.status='completed'
                 where
                     u.role = 'walker'
                 group by
-                    w_rating.walker_id;`;
+                    u.user_id;`;
 module.exports = {
     fetch_dogs_and_owner_name,
     fetch_open_walk_requests,
