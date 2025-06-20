1d1ec9d6577867d158834984e8ac2e77f35c1415
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 14:02:00 +0000

    [automatic commit] 2025-06-20 14:02:00 UTC 115.166.30.211

--- part1/data/queries.js
+++ part1/data/queries.js
@@ -29,8 +29,7 @@ const fetch_walker_summary = `
                 SELECT
                     u.username as walker_username,
                     COUNT(w_rating.rating_id) as total_ratings,
-                    AVG(w_rating.rating) ,
-                    CAST(AVG(grade.Grade) AS DECIMAL(10,2))as average_rating
+                    CAST(AVG(w_rating.rating) AS DECIMAL(10,2))as average_rating,
                     (
                         SELECT
                             COUNT(w_rating_inner.rating_id)
