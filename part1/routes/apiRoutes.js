487ad12e97fa9006db89f39ae471ac3948362e1f
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 15:14:06 +0000

    [automatic commit] 2025-06-20 15:14:05 UTC 115.166.30.211

--- part1/routes/apiRoutes.js
+++ part1/routes/apiRoutes.js
@@ -99,7 +99,7 @@ router.get('/walkers/summary', async function (req, res, next) {
                 SELECT
                     u.username as walker_username,
                     COUNT(DISTINCT w_rating.rating_id) as total_ratings,
-                    ROUND(AVG( DISTINCT w_rating.rating), 1) as average_rating,
+                    ROUND(AVG(DISTINCT w_rating.rating), 1) as average_rating,
                     COUNT(DISTINCT w_req.request_id) as completed_walks
                 from Users u
                         left join WalkRatings w_rating on u.user_id = w_rating.walker_id
