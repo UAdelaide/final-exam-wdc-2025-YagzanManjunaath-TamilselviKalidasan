00786f904131b813024d9c43fedd353f00d4ccc2
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 13:13:27 +0000

    [automatic commit] 2025-06-20 13:13:27 UTC 115.166.30.211

--- part1/data/queries.js
+++ part1/data/queries.js
@@ -1,7 +1,17 @@
 const fetch_dogs_and_owner_name = `SELECT d.name as dog_name, d.size as size, u.username as owner_username from
                                     Dogs d inner join Users u on d.owner_id = u.user_id;`;
 
-const fetch_open_walk_requests = `;`;
+/*
+  {
+"request_id": 1,
+"dog_name": "Max",
+"requested_time": "2025-06-10T08:00:00.000Z",
+"duration_minutes": 30,
+"location": "Parklands",
+"owner_username": "alice123"
+}
+*/
+const fetch_open_walk_requests = `SELECT req.request_id, d;`;
 module.exports = {
     fetch_dogs_and_owner_name,
     fetch_open_walk_requests
