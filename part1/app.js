491e511b7df980acd080c7ea1e763af93c84551d
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 08:34:57 +0000

    [automatic commit] 2025-06-20 08:34:57 UTC 115.166.30.211

--- part1/app.js
+++ part1/app.js
@@ -162,7 +162,7 @@ let db;
                         (SELECT w_req.request_id from WalkRequests w_req
                         inner join Dogs d on w_req.dog_id = d.dog_id
                         where d.name  = 'Max' LIMIT 1),
-                        (SELECT Users.u = 'Max' LIMIT 1)
+                         (SELECT Users.user_id from Users where Users.username  = 'bobwalker' LIMIT 1)
                         '2025-06-12 11:30:00',20,'Egmore grounds','open'
                         , 0, 0, 0, 0, '', CURRENT_TIMESTAMP);
 
