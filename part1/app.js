393a1fe7150e61a9226b319a74963a122b2cc1bf
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 14:43:55 +0000

    [automatic commit] 2025-06-20 14:43:55 UTC 115.166.30.211

--- part1/app.js
+++ part1/app.js
@@ -146,7 +146,17 @@ let db;
                             inner join Dogs d on w_req.dog_id = d.dog_id
                             where d.name  = 'Ace' LIMIT 1),
                             (SELECT Users.user_id from Users where Users.username  = 'bobwalker' LIMIT 1),
-                            '2025-06-10 10:00:00',
+                            '2025-06-11 18:30:00',
+                            'accepted'
+                        );`,
+            `INSERT INTO DogWalkService.WalkApplications
+                        (request_id, walker_id, applied_at, status)
+                        VALUES(
+                            (SELECT w_req.request_id from WalkRequests w_req
+                            inner join Dogs d on w_req.dog_id = d.dog_id
+                            where d.name  = 'Ace' LIMIT 1),
+                            (SELECT Users.user_id from Users where Users.username  = 'bobwalker' LIMIT 1),
+                            '2025-06-11 18:30:00',
                             'accepted'
                         );`
         ];
