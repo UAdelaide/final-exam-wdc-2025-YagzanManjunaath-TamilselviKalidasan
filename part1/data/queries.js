c0aec1077611d581cdcf9e50d4af74d617bc71ab
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 13:53:44 +0000

    [automatic commit] 2025-06-20 13:53:44 UTC 115.166.30.211

--- part1/data/queries.js
+++ part1/data/queries.js
@@ -2,12 +2,21 @@ const fetch_dogs_and_owner_name = `SELECT d.name as dog_name, d.size as size, u.
                                     Dogs d inner join Users u on d.owner_id = u.user_id;`;
 
 
-const fetch_open_walk_requests = `SELECT w_req.request_id, d.name as dog_name, w_req.requested_time,
-                                         w_req.duration_minutes,w_req.location,u.username as owner_username
-                                            from WalkRequests w_req
-                                                inner join Dogs d on w_req.dog_id = d.dog_id
-                                                inner join Users u on d.owner_id = u.user_id
-                                            where w_req.status = 'open';`;
+const fetch_open_walk_requests = `
+                SELECT
+                    w_req.request_id,
+                    d.name as dog_name,
+                    w_req.requested_time,
+                    w_req.duration_minutes,
+                    w_req.location,
+                    u.username as owner_username
+                from
+                    WalkRequests w_req
+                    inner join Dogs d on w_req.dog_id = d.dog_id
+                    inner join Users u on d.owner_id = u.user_id
+                where
+                    w_req.status = 'open';
+`;
 
 
 
