4fd441e5d60a96f57e9073e153e2c0e12927642b
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 15:09:40 +0000

    [automatic commit] 2025-06-20 15:09:40 UTC 115.166.30.211

--- part1/app.js
+++ part1/app.js
@@ -166,16 +166,16 @@ let db;
         '2025-06-12 11:45:00',
             'accepted'
                         ); `,
-        //     `INSERT INTO DogWalkService.WalkApplications
-        //     (request_id, walker_id, applied_at, status)
-        // VALUES(
-        //     (SELECT w_req.request_id from WalkRequests w_req
-        //                     inner join Dogs d on w_req.dog_id = d.dog_id
-        //                     where d.name = 'Krypto' LIMIT 1),
-        //     (SELECT Users.user_id from Users where Users.username = 'alfred' LIMIT 1),
-        // '2025-06-12 11:30:00',
-        //     'accepted'
-        //                 ); `
+            `INSERT INTO DogWalkService.WalkApplications
+            (request_id, walker_id, applied_at, status)
+        VALUES(
+            (SELECT w_req.request_id from WalkRequests w_req
+                            inner join Dogs d on w_req.dog_id = d.dog_id
+                            where d.name = 'Krypto' LIMIT 1),
+            (SELECT Users.user_id from Users where Users.username = 'alfred' LIMIT 1),
+        '2025-06-12 11:30:00',
+            'accepted'
+                        ); `
         ];
 
 
