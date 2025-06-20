89f7fc9062396d76e8f0ac65eeea88cd42afcdbb
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 12:27:57 +0000

    [automatic commit] 2025-06-20 12:27:57 UTC 115.166.30.211

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,14 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  try{
+router.get('/', function (req, res, next) {
+    try
+    {const { username, password } = req.body;
 
-  }catch(err){
-    const error_message = `Error occurred during request ${err.message}`;
-    console.err(error_message);
-    res.status(500).json({ error: 'Failed to fetch teams' });
-  }
+    } catch (err) {
+        const error_message = `Error occurred during request ${err.message}`;
+        console.err(error_message);
+        res.status(500).json({ error: error_message });
+    }
 });
 
 module.exports = router;
