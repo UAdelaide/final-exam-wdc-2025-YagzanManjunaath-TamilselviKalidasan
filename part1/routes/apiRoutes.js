8bbc0f17c6493fd896f860c3648f1acfade98dbf
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 13:56:54 +0000

    [automatic commit] 2025-06-20 13:56:54 UTC 115.166.30.211

--- part1/routes/apiRoutes.js
+++ part1/routes/apiRoutes.js
@@ -55,19 +55,19 @@ router.get('/walkrequests/open', async function (req, res, next) {
             Fetch Open walks from database using query from queries.js
                 const fetch_open_walk_requests =
                     `
-                        SELECT
-                            w_req.request_id,
-                            d.name as dog_name,
-                            w_req.requested_time,
-                            w_req.duration_minutes,
-                            w_req.location,
-                            u.username as owner_username
-                        from
-                            WalkRequests w_req
-                            inner join Dogs d on w_req.dog_id = d.dog_id
-                            inner join Users u on d.owner_id = u.user_id
-                        where
-                            w_req.status = 'open';
+                    SELECT
+                        w_req.request_id,
+                        d.name as dog_name,
+                        w_req.requested_time,
+                        w_req.duration_minutes,
+                        w_req.location,
+                        u.username as owner_username
+                    from
+                        WalkRequests w_req
+                        inner join Dogs d on w_req.dog_id = d.dog_id
+                        inner join Users u on d.owner_id = u.user_id
+                    where
+                        w_req.status = 'open';
                     `;
 
 
