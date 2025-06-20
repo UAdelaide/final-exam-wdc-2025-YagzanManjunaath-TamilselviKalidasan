5cbbaa9fac7f328c054a60c4c2c8378365ee9728
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 14:38:29 +0000

    [automatic commit] 2025-06-20 14:38:29 UTC 115.166.30.211

--- part1/data/queries.js
+++ part1/data/queries.js
@@ -35,7 +35,7 @@ const fetch_walker_summary = `
                         left join WalkRatings w_rating on u.user_id = w_rating.walker_id
                         left join WalkApplications w_appln on u.user_id = w_appln.walker_id
                             and w_appln.status = 'accepted'
-                        left join WalkRequests w_req on w_req.request_id = w_req.walker_id
+                        left join WalkRequests w_req on w_req.request_id = w_appln.request_id
                             and w_req.status = 'completed'
                 where
                     u.role = 'walker'
