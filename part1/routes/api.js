735aec7e1139ddb4669955efde82c0862e93244d
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 12:25:34 +0000

    [automatic commit] 2025-06-20 12:25:33 UTC 115.166.30.211

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  try{
+
+  }catch(err){
+    con
+  }
 });
 
 module.exports = router;
