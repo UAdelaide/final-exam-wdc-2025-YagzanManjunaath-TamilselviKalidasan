02d86b2a7e014c5bab9c32c7940d3e27c5d25abc
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 11:40:08 +0000

    [automatic commit] 2025-06-20 11:40:08 UTC 115.166.30.211

--- part1/app.js
+++ part1/app.js
@@ -87,10 +87,10 @@ let db;
         ];
         const user_insert_queries = [
             `INSERT INTO Users(username,email,password_hash,role) values ('alice123','alice@example.com','hashed123','owner');`,
-                `INSERT INTO Users(username,email,password_hash,role) values ('bobwalker','bob@example.com','hashed456','walker');`
-                `INSERT INTO Users(username,email,password_hash,role) values ('carol123','carol@example.com','hashed789','owner');
-                INSERT INTO Users(username,email,password_hash,role) values ('batman','brucewayne@example.com','martha123','owner');
-                INSERT INTO Users(username,email,password_hash,role) values ('alfred','alfred@example.com','thomas123','walker');`
+            `INSERT INTO Users(username,email,password_hash,role) values ('bobwalker','bob@example.com','hashed456','walker');`,
+            `INSERT INTO Users(username,email,password_hash,role) values ('carol123','carol@example.com','hashed789','owner');`,
+            `INSERT INTO Users(username,email,password_hash,role) values ('batman','brucewayne@example.com','martha123','owner');`,
+            `INSERT INTO Users(username,email,password_hash,role) values ('alfred','alfred@example.com','thomas123','walker');`
         ];
 
         await Promise.all(table_queries.map((query) => db.execute(query)));
