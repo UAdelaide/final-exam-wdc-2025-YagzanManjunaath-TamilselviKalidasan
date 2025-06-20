efa8f8a293cbaf8f51df843f4080efadf9261ab4
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 13:33:26 +0000

    [automatic commit] 2025-06-20 13:33:26 UTC 115.166.30.211

--- part1/data/queries.js
+++ part1/data/queries.js
@@ -8,6 +8,17 @@ const fetch_open_walk_requests = `SELECT w_req.request_id, d.name as dog_name, w
                                                 inner join Dogs d on w_req.dog_id = d.dog_id
                                                 inner join Users u on d.owner_id = u.user_id
                                             where w_req.status = 'open';`;
+
+                                            /**
+                                             *   {
+    "walker_username": "newwalker",
+    "total_ratings": 0,
+    "average_rating": null,
+    "completed_walks": 0
+  }
+                                             */
+
+const fetch_open_walk_requests = `SELECT w_req.request_id, d.name as dog_name, w_req.requested_time,
 module.exports = {
     fetch_dogs_and_owner_name,
     fetch_open_walk_requests
