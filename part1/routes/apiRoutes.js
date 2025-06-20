f1b2aa508bf330f9f5ba7de988fd4d4ce4eb3b1f
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 14:13:39 +0000

    [automatic commit] 2025-06-20 14:13:39 UTC 115.166.30.211

--- part1/routes/apiRoutes.js
+++ part1/routes/apiRoutes.js
@@ -95,29 +95,29 @@ router.get('/walkers/summary', async function (req, res, next) {
 
         /*
             Fetch summary of completed walks using query from queries.js
-                const fetch_walker_summary =
-                `
-                SELECT
-                    u.username as walker_username,
-                    COUNT(w_rating.rating_id) as total_ratings,
-                    CAST(AVG(w_rating.rating) AS DECIMAL(10,1))as average_rating,
-                    (
-                        SELECT
-                            COUNT(w_rating_inner.rating_id)
-                        from
-                            WalkRatings w_rating_inner
-                            inner join WalkRequests
-                             w_req on w_rating_inner.request_id = w_req.request_id
-                        where
-                            w_rating_inner.walker_id = w_rating.walker_id
-                            and w_req.status = 'completed'
-                    ) as completed_walks
-                from
-                    WalkRatings w_rating
-                    inner join Users u on u.user_id = w_rating.walker_id
-                group by
-                    w_rating.walker_id;
-                `;
+                const fetch_walker_summary = `
+                    SELECT
+                        u.username as walker_username,
+                        COUNT(w_rating.rating_id) as total_ratings,
+                        ROUND(AVG(wr.rating), 1) as average_rating,
+                        (
+                            SELECT
+                                COUNT(w_rating_inner.rating_id)
+                            from
+                                WalkRatings w_rating_inner
+                                inner join WalkRequests w_req
+                                    on w_rating_inner.request_id = w_req.request_id
+                            where
+                                w_rating_inner.walker_id = w_rating.walker_id
+                                and w_req.status = 'completed'
+                        ) as completed_walks
+                    from
+                        WalkRatings w_rating
+                        inner join Users u on u.user_id = w_rating.walker_id
+                    where
+                        u.role = 'walker'
+                    group by
+                        w_rating.walker_id;`;
 
         */
         let fetch_walker_summary_result = await query(fetch_walker_summary);
