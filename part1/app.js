319dc704f64d2c6bf9df841ef444009f67c4889d
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 12:56:32 +0000

    [automatic commit] 2025-06-20 12:56:32 UTC ;; communications error to 208.67.222.222#53: timed out 115.166.30.211

--- part1/app.js
+++ part1/app.js
@@ -32,7 +32,7 @@ let db;
         });
 
         const table_queries = [
-            `CREATE TABLE Users (
+            `CREATE TABLE  IF NOT EXISTS Users (
                 user_id INT AUTO_INCREMENT PRIMARY KEY,
                 username VARCHAR(50) UNIQUE NOT NULL,
                 email VARCHAR(100) UNIQUE NOT NULL,
