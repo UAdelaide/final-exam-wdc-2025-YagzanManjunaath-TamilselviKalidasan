5352edd493865a850ab18aad0d2146f2150cb3f5
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 13:09:58 +0000

    [automatic commit] 2025-06-20 13:09:58 UTC 115.166.30.211

--- part1/routes/apiRoutes.js
+++ part1/routes/apiRoutes.js
@@ -34,7 +34,38 @@ router.get('/dogs', async function (req, res, next) {
     }
 });
 
-// /api/walkrequests/open
+//
 
+
+/* GET All Dogs information */
+router.get('/walkrequests/open', async function (req, res, next) {
+    try {
+
+        /*
+            Fetch Dogs from database using query from queries.js
+            const fetch_dogs_and_owner_name =
+            `
+            SELECT d.name as dog_name, d.size as size, u.name as owner_username
+                from Dogs d inner join Users u on d.owner_id = u.user_id
+            `
+        */
+        let dogs_with_owner_name_result = await query(fetch_dogs_and_owner_name);
+        /*
+            If no dogs are in the database , return 204 No content with emnpty body
+        */
+        if (dogs_with_owner_name_result && dogs_with_owner_name_result.length === 0) {
+            return res.status(204).send();
+        }
+        /*
+           If found, return list as Json with status code 200
+        */
+        return res.status(200).json(dogs_with_owner_name_result);
+
+    } catch (err) {
+        const error_message = `Error occurred during request ${err.message}`;
+        console.error(`ERROR : ${error_message}`);
+        return res.status(500).json({ error: error_message });
+    }
+});
 
 module.exports = router;
