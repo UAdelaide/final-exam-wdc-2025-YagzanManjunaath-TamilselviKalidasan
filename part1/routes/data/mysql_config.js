9b721b6e4352cddffc7303e302730db10728f76e
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 12:37:19 +0000

    [automatic commit] 2025-06-20 12:37:19 UTC 115.166.30.211

--- part1/routes/data/mysql_config.js
+++ part1/routes/data/mysql_config.js
@@ -10,11 +10,11 @@ const pool = mysql.createPool({
 
 const query = (sql, args) => {
     return new Promise((resolve, reject) => {
-        pool.getConnection(function (err, connection){
+        pool.getConnection(function (err, connection) {
             if (err) {
                 return reject(err);
             }
-            connection.query(sql, args,function (err, result){
+            connection.query(sql, args, function (err, result) {
                 connection.release();
                 if (err) {
                     return reject(err);
@@ -24,4 +24,4 @@ const query = (sql, args) => {
         });
 
     });
-}
+};
