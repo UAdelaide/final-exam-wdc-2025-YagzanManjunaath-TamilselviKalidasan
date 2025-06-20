f0c0ad278b7fc615e123b315a43d27894dce7830
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 14:53:16 +0000

    [automatic commit] 2025-06-20 14:53:16 UTC 115.166.30.211

--- part1/app.js
+++ part1/app.js
@@ -130,9 +130,9 @@ let db;
             `INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status) values(
                 (SELECT Dogs.dog_id from Dogs where Dogs.name = 'Sugar' LIMIT 1), '2025-06-12 11:30:00', 20, 'Egmore grounds', 'completed'
                     ); `,
-            `INSERT INTO WalkRequests(dog_id,requested_time,duration_minutes,location,status) values (
-                        (SELECT Dogs.dog_id from Dogs where Dogs.name  = 'Ace' LIMIT 1),'2025-06-12 18:30:00',45,'Wayne Manor grounds','open'
-                    );`,
+            `INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status) values(
+                (SELECT Dogs.dog_id from Dogs where Dogs.name = 'Sugar' LIMIT 1), '2025-06-13 11:30:00', 20, 'Egmore grounds', 'completed'
+                    ); `
         ];
 
         const insert_walk_applications_queries = [
