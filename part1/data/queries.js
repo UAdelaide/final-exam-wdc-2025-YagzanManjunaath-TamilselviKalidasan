62221a6456ef9113f82e704560daa711a1ec96ec
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 14:35:19 +0000

    [automatic commit] 2025-06-20 14:35:19 UTC 115.166.30.211

--- part1/data/queries.js
+++ part1/data/queries.js
@@ -26,19 +26,22 @@ const fetch_open_walk_requests = `
 `;
 
 const fetch_walker_summary = `
-                SELECT
-                    u.username as walker_username,
-                    COUNT(w_rating.rating_id) as total_ratings,
-                    ROUND(AVG(w_rating.rating), 1) as average_rating,
-                    COUNT(w_req.request_id) as completed_walks
-                from Users u
-                        left join WalkRatings w_rating on u.user_id = w_rating.walker_id
-                        left join WalkRequests w_req on w_rating.request_id = w_req.request_id
-                        and w_req.status='completed'
-                where
-                    u.role = 'walker'
-                group by
-                    u.user_id;`;
+SELECT
+    u.username AS walker_username,
+    COUNT(wr.rating_id) AS total_ratings,
+    ROUND(AVG(wr.rating), 1) AS average_rating,
+    COUNT(rq.request_id) AS completed_walks
+FROM
+    Users u
+    LEFT JOIN WalkRatings wr ON wr.walker_id = u.user_id
+    LEFT JOIN WalkApplications wa ON wa.walker_id = u.user_id
+    AND wa.status = 'accepted'
+    LEFT JOIN WalkRequests rq ON rq.request_id = wa.request_id
+    AND rq.status = 'completed'
+WHERE
+    u.role = 'walker'
+GROUP BY
+    u.user_id;`;
 module.exports = {
     fetch_dogs_and_owner_name,
     fetch_open_walk_requests,
--- part1/test.sql
+++ part1/test.sql
@@ -1,21 +1,16 @@
 SELECT
-    u.username as walker_username,
-    COUNT(w_rating.rating_id) as total_ratings,
-    ROUND(AVG(wr.rating), 1) as average_rating,
-    (
-        SELECT
-            COUNT(w_rating_inner.rating_id)
-        from
-            WalkRatings w_rating_inner
-            inner join WalkRequests w_req on w_rating_inner.request_id = w_req.request_id
-        where
-            w_rating_inner.walker_id = w_rating.walker_id
-            and w_req.status = 'completed'
-    ) as completed_walks
-from
-    WalkRatings w_rating
-    inner join Users u on u.user_id = w_rating.walker_id
-where
+    u.username AS walker_username,
+    COUNT(wr.rating_id) AS total_ratings,
+    ROUND(AVG(wr.rating), 1) AS average_rating,
+    COUNT(rq.request_id) AS completed_walks
+FROM
+    Users u
+    LEFT JOIN WalkRatings wr ON wr.walker_id = u.user_id
+    LEFT JOIN WalkApplications wa ON wa.walker_id = u.user_id
+    AND wa.status = 'accepted'
+    LEFT JOIN WalkRequests rq ON rq.request_id = wa.request_id
+    AND rq.status = 'completed'
+WHERE
     u.role = 'walker'
-group by
-    w_rating.walker_id;
+GROUP BY
+    u.user_id;
