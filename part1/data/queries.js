5e550574371414a8f1b4827b210f55cb5f8542ae
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 13:47:56 +0000

    [automatic commit] 2025-06-20 13:47:56 UTC 115.166.30.211

--- part1/data/queries.js
+++ part1/data/queries.js
@@ -22,7 +22,7 @@ const fetch_walker_summary = `SELECT u.username as walker_username, COUNT(w_rati
                                     AVG(w_rating.rating) as average_rating,
                                     (SELECT COUNT(w_rating_inner.rating_id) from WalkRatings w_rating_inner
                                                 inner join WalkRequests w_req on w_rating_inner.request_id = w_req.request_id
-                                            where w_rating_inner.walker_id = w_rating.walker_id and w_rating_inner.status = 'completed')
+                                            where w_rating_inner.walker_id = w_rating.walker_id and w_req.status = 'completed')
                                      as completed_walks
                                 from WalkRatings w_rating
                                     inner join Users u on u.user_id = w_rating.walker_id
