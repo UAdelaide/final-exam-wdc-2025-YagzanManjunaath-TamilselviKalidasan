52772f557f5499baf2e62b32c8c0e183cadb872c
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 14:35:40 +0000

    [automatic commit] 2025-06-20 14:35:40 UTC 115.166.30.211

--- part1/data/queries.js
+++ part1/data/queries.js
@@ -26,22 +26,22 @@ const fetch_open_walk_requests = `
 `;
 
 const fetch_walker_summary = `
-SELECT
-    u.username AS walker_username,
-    COUNT(wr.rating_id) AS total_ratings,
-    ROUND(AVG(wr.rating), 1) AS average_rating,
-    COUNT(rq.request_id) AS completed_walks
-FROM
-    Users u
-    LEFT JOIN WalkRatings wr ON wr.walker_id = u.user_id
-    LEFT JOIN WalkApplications wa ON wa.walker_id = u.user_id
-    AND wa.status = 'accepted'
-    LEFT JOIN WalkRequests rq ON rq.request_id = wa.request_id
-    AND rq.status = 'completed'
-WHERE
-    u.role = 'walker'
-GROUP BY
-    u.user_id;`;
+                SELECT
+                    u.username AS walker_username,
+                    COUNT(wr.rating_id) AS total_ratings,
+                    ROUND(AVG(wr.rating), 1) AS average_rating,
+                    COUNT(rq.request_id) AS completed_walks
+                FROM
+                    Users u
+                    LEFT JOIN WalkRatings wr ON wr.walker_id = u.user_id
+                    LEFT JOIN WalkApplications wa ON wa.walker_id = u.user_id
+                    AND wa.status = 'accepted'
+                    LEFT JOIN WalkRequests rq ON rq.request_id = wa.request_id
+                    AND rq.status = 'completed'
+                WHERE
+                    u.role = 'walker'
+                GROUP BY
+                    u.user_id;`;
 module.exports = {
     fetch_dogs_and_owner_name,
     fetch_open_walk_requests,
