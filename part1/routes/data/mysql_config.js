b56edf543b3f48a27c9922f8aedbd2237f1432ae
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 12:33:49 +0000

    [automatic commit] 2025-06-20 12:33:49 UTC 115.166.30.211

--- part1/routes/data/mysql_config.js
+++ part1/routes/data/mysql_config.js
@@ -1,8 +1,9 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-host: '127.0.0.1',
-user:'root',
-password:'',
-
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'DogWalkService',
+    connectionLimit: 10
 });
