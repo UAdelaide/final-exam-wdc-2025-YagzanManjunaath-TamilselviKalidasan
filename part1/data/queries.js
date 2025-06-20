1ff5bddf94eb81e64eb4194080f26486a088a9e1
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 13:41:43 +0000

    [automatic commit] 2025-06-20 13:41:43 UTC 115.166.30.211

--- part1/data/queries.js
+++ part1/data/queries.js
@@ -18,12 +18,12 @@ const fetch_open_walk_requests = `SELECT w_req.request_id, d.name as dog_name, w
 }
  */
 
-const fetch_walker_summary = `SELECT u.username as walker_username, COUNT(ratings_id) as total_ratings,
-                                    AVG(rating) as average_rating,
-                                    (SELECT COUNT(ratings_id) from WalkRatings wr_inner
+const fetch_walker_summary = `SELECT u.username as walker_username, COUNT(w_rating.ratings_id) as total_ratings,
+                                    AVG(w_rating.rating) as average_rating,
+                                    (SELECT COUNT(wr_inner.ratings_id) from WalkRatings wr_inner
                                             where wr_inner.walker_id = w_rating.walker_id and wr_inner.status = 'completed')
-                                     as completed_walks
-                                from WalkRatings 
+                                     as completed_walks w_rating
+                                from WalkRatings
                                      group by walker
                                      ;`;
 module.exports = {
