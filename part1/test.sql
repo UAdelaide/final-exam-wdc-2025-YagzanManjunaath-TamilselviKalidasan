8044fba562d361d20ef8358028a5f51714912d41
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 13:53:24 +0000

    [automatic commit] 2025-06-20 13:53:24 UTC 115.166.30.211

--- part1/test.sql
+++ part1/test.sql
@@ -1,19 +1,13 @@
 SELECT
-    u.username as walker_username,
-    COUNT(w_rating.rating_id) as total_ratings,
-    AVG(w_rating.rating) as average_rating,
-    (
-        SELECT
-            COUNT(w_rating_inner.rating_id)
-        from
-            WalkRatings w_rating_inner
-            inner join WalkRequests w_req on w_rating_inner.request_id = w_req.request_id
-        where
-            w_rating_inner.walker_id = w_rating.walker_id
-            and w_req.status = 'completed'
-    ) as completed_walks
+    w_req.request_id,
+    d.name as dog_name,
+    w_req.requested_time,
+    w_req.duration_minutes,
+    w_req.location,
+    u.username as owner_username
 from
-    WalkRatings w_rating
-    inner join Users u on u.user_id = w_rating.walker_id
-group by
-    w_rating.walker_id;
+    WalkRequests w_req
+    inner join Dogs d on w_req.dog_id = d.dog_id
+    inner join Users u on d.owner_id = u.user_id
+where
+    w_req.status = 'open';
