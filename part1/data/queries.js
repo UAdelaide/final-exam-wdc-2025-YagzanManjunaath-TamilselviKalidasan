7a6bf78ecb808ee80bcc51f0076501d279f7247d
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-20 14:37:02 +0000

    [automatic commit] 2025-06-20 14:37:02 UTC 115.166.30.211

--- part1/data/queries.js
+++ part1/data/queries.js
@@ -33,6 +33,7 @@ const fetch_walker_summary = `
                     COUNT(w_req.request_id) as completed_walks
                 from Users u
                         left join WalkRatings w_rating on u.user_id = w_rating.walker_id
+                        left join WalkApplications w_appln 
                         left join WalkRequests w_req on w_rating.user_id = w_req.walker_id
                         and w_req.status='completed'
                 where
