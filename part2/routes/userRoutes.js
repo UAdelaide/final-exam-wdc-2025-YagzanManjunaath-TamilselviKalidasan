9f24d9635acc21cfaab129c88fc01e51be79ee3c
Yagzan Manjunaath Tamilselvi Kalidasan <a1932901@student.adelaide.edu.au>
2025-06-21 04:03:18 +0000

    [automatic commit] 2025-06-21 04:03:18 UTC 115.166.30.211

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -50,6 +50,7 @@ body('password')
 
 ], async (req, res) => {
 
+  
    const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
